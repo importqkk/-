9bd093112256b30e10d02cf38d1a49a0fbcc68fe
importqkk <import_qkk@naver.com>
2023-03-23 17:54:03 +0900

    장바구니 dao (230323)

--- semiproject/src/main/java/com/kh/semi/dao/CartDao.java
+++ semiproject/src/main/java/com/kh/semi/dao/CartDao.java
@@ -19,52 +19,45 @@ public class CartDao {
 		@Override
 		public CartDto mapRow(ResultSet rs, int rowNum) throws SQLException {
 			return CartDto.builder()
-						.cartNo(rs.getInt("cart_no"))
-						.cartCount(rs.getInt("cart_count"))
-						.cartPrice(rs.getInt("cart_price"))
-						.productNo(rs.getInt("product_no"))
 						.memberId(rs.getString("member_id"))
+						.productNo(rs.getInt("product_no"))
+						.productCount(rs.getInt("product_count"))
 					.build();
 		}
 	};
 	
 	// 장바구니에 저장(insert)
 	public void cartInsert(CartDto cartDto) {
-		String sql = "insert into cart values(cart_seq.nextval, ?, ?, ?, ?)";
-		Object[] param = {cartDto.getCartCount(), cartDto.getCartPrice(),
-						cartDto.getProductNo(), cartDto.getMemberId()};
+		String sql = "insert into cart values(?, ?, ?)";
+		Object[] param = {cartDto.getMemberId(), cartDto.getProductNo(), 
+						cartDto.getProductCount()};
 		jdbcTemplate.update(sql, param);
 	}
 	
-	// 해당 사용자의 장바구니 찾기(select)
+	// 장바구니 조회(list)
 	public List<CartDto> cartList(String memberId) {
 		String sql = "select * from cart where member_id=?";
 		Object[] param = {memberId};
 		return jdbcTemplate.query(sql, mapper, param);
 	}
 	
-	// 장바구니 내의 상품 찾기
-//	public ProductDto cartItem(String memberId) {
-//		String sql = "select * from product where product_no = "
-//					+ "(select product_no from cart where member_id=?)";
-//		Object[] param = {memberId};
-//		List<CartDto> list = jdbcTemplate.query(sql, mapper, param);
-//		return list.isEmpty() ? null : list.get(0);
-//	}
-	
 	// 장바구니 상품 개수 변경(update - set)
-	public boolean cartIncreaseCount(CartDto cartDto) {
-		String sql = "update cart set cart_count=? "
-					+ "where cart_no=? and product_no=?";
-		Object[] param = {cartDto.getCartNo(), cartDto.getProductNo()};
+	public boolean cartChangeCount(CartDto cartDto) {
+		String sql = "update cart set product_count=? "
+					+ "where member_id=? and product_no=?";
+		Object[] param = {cartDto.getProductCount(), cartDto.getMemberId(), 
+						cartDto.getProductNo()};
 		return jdbcTemplate.update(sql, param) > 0;
 	}
 	
 	// 장바구니에서 상품 삭제(delete)
-	public boolean cartDeleteItem(int productNo) {
-		String sql = "delete * from cart where product_no=?";
-		Object[] param = {productNo};
+	public boolean cartDeleteItem(CartDto cartDto) {
+		String sql = "delete cart where member_id=? and product_no=?";
+		Object[] param = {cartDto.getMemberId(), cartDto.getProductNo()};
 		return jdbcTemplate.update(sql, param) > 0;
 	}
+	
+	// 장바구니에 있는 상품 개수 갱신 기능 (뷰)
+	// 금액 정보 업데이트 기능 (뷰)
 	
 }
