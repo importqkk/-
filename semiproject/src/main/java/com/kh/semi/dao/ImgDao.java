bf14149c2e4246f82f161b3c0e66b7f4270ca3d0
wndudwhdka <wndudwhdka@naver.com>
2023-03-24 12:42:16 +0900

    상세페이지 변경

--- semiproject/src/main/java/com/kh/semi/dao/ImgDao.java
+++ semiproject/src/main/java/com/kh/semi/dao/ImgDao.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import com.kh.semi.dto.ImageDto;
 import com.kh.semi.dto.ImgDto;
 @Repository
 public class ImgDao {
