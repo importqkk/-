f89c5a67d0d6008ca9029f84c305031bc35abf60
importqkk <import_qkk@naver.com>
2023-03-24 17:58:37 +0900

    회원 dto (merge 금지)

---
+++ semiproject/src/main/java/com/kh/semi/dto/MemberDto.java
@@ -0,0 +1,21 @@
+package com.kh.semi.dto;
+import java.sql.Date;
+import lombok.Data;
+
+@Data
+public class MemberDto {
+	private String memberId;
+	private String memberName;
+	private String memberNick;
+	private String memberPw;
+	private String memberPhone;
+	private String memberEmail;
+	private int memberPost;
+	private String memberBasicAddr;
+	private String memberDetailAddr;
+	private int memberPoint;
+	private Date memberJoin;
+	private String memberRole;	
+	
+	
+}
