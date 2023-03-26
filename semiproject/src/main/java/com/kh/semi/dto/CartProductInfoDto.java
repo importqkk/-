560d4d0de44fc96d4be9af9811c209244490b629
importqkk <import_qkk@naver.com>
2023-03-26 16:24:28 +0900

    장바구니-상품 dto (230326)

--- semiproject/src/main/java/com/kh/semi/dto/CartProductInfoDto.java
+++ semiproject/src/main/java/com/kh/semi/dto/CartProductInfoDto.java
@@ -11,4 +11,16 @@ public class CartProductInfoDto {
 	private String memberId, productBrand, productName;
 	private int productNo, productPrice, productStock;
 	
+	/*public int getProductPrice() {
+		return productPrice += productPrice;
+	}
+	
+	public int getDeleveryPrice() {
+		return 3000;
+	}
+	
+	public int getTotalPrice() {
+		return 3000 + getProductPrice();
+	}*/
+	
 }
