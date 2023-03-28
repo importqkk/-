e9efa269fbfa643799d06d2ea179623ef53cf045
importqkk <import_qkk@naver.com>
2023-03-28 11:03:38 +0900

    파일 업로드 프로퍼티 (230328)

---
+++ semiproject/src/main/java/com/kh/semi/configuration/FileUploadProperties.java
@@ -0,0 +1,20 @@
+package com.kh.semi.configuration;
+import java.io.File;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import lombok.Data;
+
+@Data
+@Component
+// 컴포넌트 선언
+@ConfigurationProperties(prefix = "fileupload")
+// application.properties에서 custom.fileupload 설정을 가져오는 어노테이션
+public class FileUploadProperties {
+
+	private String path; 
+	
+	public File getDirectory() {
+		return new File(path);
+	}
+	
+}
