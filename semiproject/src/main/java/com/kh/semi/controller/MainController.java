a35c44d552a09d0bbdc39e3db135c501264c7218
importqkk <import_qkk@naver.com>
2023-03-29 17:40:24 +0900

    메인 컨트롤러 (230329)

--- semiproject/src/main/java/com/kh/semi/controller/MainController.java
+++ semiproject/src/main/java/com/kh/semi/controller/MainController.java
@@ -1,12 +1,35 @@
 package com.kh.semi.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.kh.semi.dao.ProductInfoDao;
+import com.kh.semi.dto.ProductInfoDto;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private ProductInfoDao productInfoDao;
+	
 	@RequestMapping("/")
-	public String main() {
+	public String main(@ModelAttribute ProductInfoDto productInfoDto,
+			Model model) {
+		
+		/*List<CartProductInfoDto> itemInfo = cartProductInfoDao.cartItemInfo(memberId);
+		int cartCnt = cartDao.cartCnt(memberId);
+		model.addAttribute("itemInfo", itemInfo);
+		model.addAttribute("cartCnt", cartCnt);*/
+		
+		// 개별 상품 정보
+		// 최신상품 정렬
+		List<ProductInfoDto> newProduct = productInfoDao.newList();
+		model.addAttribute("newProduct", newProduct);
+		// 베스트상품 정렬
+		List<ProductInfoDto> bestProduct = productInfoDao.bestList();
+		model.addAttribute("bestProduct", bestProduct);
 		return "/WEB-INF/views/main.jsp";
 	}
 	
