80e3c4603086ad33a9eab7766ea2e425b932c78e
wndudwhdka <wndudwhdka@naver.com>
2023-03-29 18:18:32 +0900

    jsp 만들다가 하루가 다가네~ 내일은 꼭 4xN 형태로 목록 출력할 것.

--- semiproject/src/main/java/com/kh/semi/controller/CategoriController.java
+++ semiproject/src/main/java/com/kh/semi/controller/CategoriController.java
@@ -23,14 +23,15 @@ public class CategoriController {
 	@Autowired
 	private ProductTagDao productTagDao;
 	
-	
 	// 전체
 	@GetMapping("/all")
-	public String all(Model model) {
+	public String all(Model model) { 
 		
-		// 모든 제품들을 select해서 보여준다.
+		// 카테고리중, 모든 제품들을 select해서 list로 생성한다. 
 		List<ProductDto> list = productDao.selectAll();
+		// all.jsp 측으로 list형태로 보내기
 		model.addAttribute("list",list);
+		
 		return "/WEB-INF/views/categori/all.jsp";
 	}
 	
@@ -44,7 +45,7 @@ public class CategoriController {
 		return "/WEB-INF/views/categori/tag1.jsp";
 	}
 	
-	// 태그 2번 노화
+	// 태그 2번 다이어트
 	@GetMapping("/tag2")
 	public String tag2(Model model) {
 		
@@ -52,7 +53,7 @@ public class CategoriController {
 		model.addAttribute("list",list);
 		return "/WEB-INF/views/categori/tag2.jsp";
 	}
-	// 태그 3번 피로
+	// 태그 3번 여성
 	@GetMapping("/tag3")
 	public String tag3(Model model) {
 		
@@ -60,7 +61,7 @@ public class CategoriController {
 		model.addAttribute("list",list);
 		return "/WEB-INF/views/categori/tag3.jsp";
 	}
-	// 태그 4번 면역력
+	// 태그 4번 활력
 	@GetMapping("/tag4")
 	public String tag4(Model model) {
 		
@@ -68,7 +69,7 @@ public class CategoriController {
 		model.addAttribute("list",list);
 		return "/WEB-INF/views/categori/tag4.jsp";
 	}
-	// 태그 5번 모발	
+	// 태그 5번 남성
 	@GetMapping("/tag5")
 	public String tag5(Model model) {
 		
@@ -84,7 +85,7 @@ public class CategoriController {
 		model.addAttribute("list",list);
 		return "/WEB-INF/views/categori/tag6.jsp";
 	}
-	// 태그 7번 간
+	// 태그 7번 치아
 	@GetMapping("/tag7")
 	public String tag7(Model model) {
 		
@@ -92,7 +93,7 @@ public class CategoriController {
 		model.addAttribute("list",list);
 		return "/WEB-INF/views/categori/tag7.jsp";
 	}
-	// 태그 8번 관절/뼈
+	// 태그 8번 뼈
 	@GetMapping("/tag8")
 	public String tag8(Model model) {
 		
