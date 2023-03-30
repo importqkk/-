d24864804959c7802fa87c61426719de60919fe3
importqkk <import_qkk@naver.com>
2023-03-30 18:47:20 +0900

    상세 이미지 dto (230330)

---
+++ semiproject/src/main/java/com/kh/semi/dto/DetailImgDto.java
@@ -0,0 +1,13 @@
+package com.kh.semi.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor @Builder
+public class DetailImgDto {
+
+	private int productNo, imgNo;
+	
+}
