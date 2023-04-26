ebc4ba924b77a1ea50951f9046d1102de176bacf
importqkk <import_qkk@naver.com>
2023-04-26 18:10:07 +0900

    임포트 수정

--- semiproject/src/main/java/com/kh/semi/controller/QaController.java
+++ semiproject/src/main/java/com/kh/semi/controller/QaController.java
@@ -1,15 +1,12 @@
 package com.kh.semi.controller;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,7 +19,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.kh.semi.advice.RequirePermissionException;
 import com.kh.semi.dao.QaDao;
 import com.kh.semi.dto.QaDto;
